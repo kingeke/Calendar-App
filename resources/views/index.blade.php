f91044265633bc49a849a12eee55e8866c1db643
Chinonso Eke <Chinonsoeke@gmail.com>
2019-11-03 16:54:42 +0100

    Initial Commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,9 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta name="description" content="Web site created using create-react-app" />
-    <link rel="apple-touch-icon" href="logo192.png" />
-    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
     <!-- Datetime picker CSS -->
     <link href="{{ asset('lib/bootstrap-datetimepicker/bootstrap-datetimepicker.css') }}" rel="stylesheet">
